4c23c815473804fc7c6869d2abed9f6975a32844
Peter Levinsky <PELE@zealand.dk>
2023-02-16 12:59:59 +0100

    Including Reading config-file (as xml)

--- ClassDemoTracing/Program.cs
+++ ClassDemoTracing/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Diagnostics;
+using System.Xml;
 
 Console.WriteLine("Hello, World!");
 
@@ -10,16 +11,17 @@ ts.Switch = new SourceSwitch("Peters Log", "All");
 TraceListener listener = new ConsoleTraceListener();
 ts.Listeners.Add(listener);
 
-TraceListener filListener = new TextWriterTraceListener(new StreamWriter("TraceDemo.txt"));
+TraceListener filListener = new TextWriterTraceListener(new StreamWriter("TraceDemo.txt") { AutoFlush=true});
 ts.Listeners.Add(filListener);
 
-TraceListener xmlListener = new XmlWriterTraceListener(new StreamWriter("TraceDemo.xml"));
+TraceListener xmlListener = new XmlWriterTraceListener(new StreamWriter("TraceDemo.xml") { AutoFlush = true });
 xmlListener.Filter = new EventTypeFilter(SourceLevels.Error);
 ts.Listeners.Add(xmlListener);
 
 
-
-
+TraceListener eventLogListener = new EventLogTraceListener("Application");
+ts.Listeners.Add(eventLogListener);
+Trace.AutoFlush= true;
 
 
 
@@ -31,4 +33,32 @@ ts.TraceEvent(TraceEventType.Warning, 567, "This is warning");
 ts.TraceEvent(TraceEventType.Error, 567, "This is error");
 ts.TraceEvent(TraceEventType.Critical, 567, "This is critical");
 
-ts.Close();
+//ts.Flush();
+//ts.Close();
+
+
+
+
+/*
+ * Configuration File
+ */
+XmlDocument xml = new XmlDocument();
+xml.Load("Config.file.xml");
+
+
+Console.WriteLine("name");
+XmlNode? nameNode = xml.DocumentElement.SelectSingleNode("Name");
+if (nameNode is not null)
+{
+    String str = nameNode.InnerText;
+    Console.WriteLine(str);
+}
+
+Console.WriteLine("number");
+XmlNode? numberNode = xml.DocumentElement.SelectSingleNode("Number");
+if (numberNode is not null)
+{
+    String str = numberNode.InnerText;
+    int number = int.Parse(str);
+    Console.WriteLine(number);
+}
