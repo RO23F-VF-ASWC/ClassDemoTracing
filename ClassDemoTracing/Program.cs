a20f1ab816f63f5798b4f407c3795c4cda73758e
Peter Levinsky <PELE@zealand.dk>
2023-02-16 09:57:18 +0100

    Add project files.

---
+++ ClassDemoTracing/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+
+Console.WriteLine("Hello, World!");
+
+
+TraceSource ts = new TraceSource("Peters Demo");
+ts.Switch = new SourceSwitch("Peters Log", "All");
+
+TraceListener listener = new ConsoleTraceListener();
+ts.Listeners.Add(listener);
+
+TraceListener filListener = new TextWriterTraceListener(new StreamWriter("TraceDemo.txt"));
+ts.Listeners.Add(filListener);
+
+TraceListener xmlListener = new XmlWriterTraceListener(new StreamWriter("TraceDemo.xml"));
+xmlListener.Filter = new EventTypeFilter(SourceLevels.Error);
+ts.Listeners.Add(xmlListener);
+
+
+
+
+
+
+
+
+
+
+ts.TraceEvent(TraceEventType.Information, 567, "This is information");
+ts.TraceEvent(TraceEventType.Warning, 567, "This is warning");
+ts.TraceEvent(TraceEventType.Error, 567, "This is error");
+ts.TraceEvent(TraceEventType.Critical, 567, "This is critical");
+
+ts.Close();
